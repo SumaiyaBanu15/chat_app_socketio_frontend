d660394bc8e40bd4eb986ae1fcacc524365ea13d
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2024-06-09 19:59:58 +0530

    Backend_connected

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:8000";
+// export const host = "http://localhost:8000"
+export const host = 'https://chat-app-socketio-serverside.onrender.com';
 export const signupRoute = `${host}/api/auth/signup`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
